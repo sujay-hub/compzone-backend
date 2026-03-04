0c31f03b96958968d13fa3961beea0e99dd883dc
sujay-hub <ssujay93@gmail.com>
2026-03-04 15:37:09 +0530

    jwtauthfilter file doFilterInternal changed.

--- src/main/java/com/project1/ecommerce/compstore/security/JWTAuthenticationFilter.java
+++ src/main/java/com/project1/ecommerce/compstore/security/JWTAuthenticationFilter.java
@@ -28,24 +28,12 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException, java.io.IOException {
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
 
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
-        String path = request.getRequestURI();
-        
-        if(
-        	path.startsWith("/login") || 
-        	path.startsWith("/register") ||
-        	path.equals("/products") ||
-        	path.equals("/categories") ||
-        	path.equals("/pagination") ||
-        	path.startsWith("/css") ||
-            path.startsWith("/js")) {
-        	filterChain.doFilter(request, response);
-        	return;
-        }
 
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
@@ -54,10 +42,19 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
             UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+
             if (jwtUtil.validateToken(token, userDetails.getUsername())) {
                 UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities()
+                        );
+
+                authToken.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
